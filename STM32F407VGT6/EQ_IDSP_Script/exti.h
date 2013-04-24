4c2b8ea075be0c7766edbd88a9631316069e2718
prid77 <prid77@naver.com>
2013-04-25 00:08:14 +0900

    Revert "20130424 on STM32F407VGT6"
    This reverts commit fcc5e12a8750f0b0fbe4ac3701d576048329a5e1.

--- STM32F407VGT6/EQ_IDSP_Script/exti.h
+++
@@ -1,8 +0,0 @@
-/* Includes ------------------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/  
-/* Private functions ---------------------------------------------------------*/
-
-void EXTILine4_Configuration(void);
-void EXTILine5_Configuration(void);
-void EXTILine6_Configuration(void);
--- STM32F407VGT6/EQ_IDSP_Script/rtc.h
+++
@@ -1,5 +0,0 @@
-/* Private functions ---------------------------------------------------------*/
-void RTC_Config(void);
-void RTC_TimeShow(void);
-void RTC_AlarmShow(void);
-void RTC_Configuration(void);
